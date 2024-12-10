409018eba42c63f30f66421f34282b8d34edd9e2
Doflamingo3D <148038248+Doflamingo3D@users.noreply.github.com>
2024-12-10 17:00:08 +0800

    Update popup.js

--- src/popup.js
+++ src/popup.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-	chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, 'updateRequest', UpdateGame);
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'updateRequest' }, UpdateGame);
     });
-
 }, false);
 
-
 function UpdateGame(response) {
-  let gameState = JSON.parse(response.gameState);
-  document.getElementById('bank-lumber-count').innerText = gameState.bank.lumber;
-  document.getElementById('bank-brick-count').innerText = gameState.bank.brick;
-  document.getElementById('bank-wool-count').innerText = gameState.bank.wool;
-  document.getElementById('bank-grain-count').innerText = gameState.bank.grain;
-  document.getElementById('bank-ore-count').innerText = gameState.bank.ore;
-  document.getElementById('bank-development-card-count').innerText = gameState.bank.devCard;
+    if (response && response.gameState) {
+        let gameState = JSON.parse(response.gameState);
+        document.getElementById('bank-lumber-count').innerText = gameState.bank.lumber;
+        document.getElementById('bank-brick-count').innerText = gameState.bank.brick;
+        document.getElementById('bank-wool-count').innerText = gameState.bank.wool;
+        document.getElementById('bank-grain-count').innerText = gameState.bank.grain;
+        document.getElementById('bank-ore-count').innerText = gameState.bank.ore;
+        document.getElementById('bank-development-card-count').innerText = gameState.bank.devCard;
+    } else {
+        console.error('Failed to receive game state:', response);
+    }
 }
